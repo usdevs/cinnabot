480b21b796066a0a220d2811e5490067dbdc3c4f
Shao Yang <hongsy2006@gmail.com>
2019-10-13 20:57:50 +0800

    Add basic deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+GOPATH=/root/go
+CINNABOT=$GOPATH/src/github.com/usdevs/cinnabot
+cd $CINNABOT
+
+# pull from github
+git pull origin master
+COMMITHEAD=$(git rev-parse --short HEAD)
+LASTUPDATED=$(git log -1 --format=%cr)
+
+# build binary
+cd main
+go build
+
+# run binary
+./main
